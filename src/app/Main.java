2938cc39d5164c6d12fc3578355947a86e85f090
Vitalii <vitaliti.yurchenko@gmail.com>
2024-11-23 21:52:27 +0200

    Added grouping by category

---
+++ src/app/Main.java
@@ -0,0 +1,23 @@
+package app;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Product> products = Arrays.asList(
+                new Product("Laptop", "Electronics", 1200.0),
+                new Product("Coffee Maker", "Appliances", 80.0),
+                new Product("Headphones", "Electronics", 150.0),
+                new Product("Blender", "Appliances", 50.0)
+        );
+
+        // Групування по категоріям
+        Map<String, List<Product>> groupedByCategory = products.stream()
+                .collect(Collectors.groupingBy(Product::getCategory));
+
+
+    }
+}
