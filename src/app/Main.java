01495f47cd25448b4027b4748d090ff07709a721
Vitalii <vitaliti.yurchenko@gmail.com>
2024-11-23 22:48:11 +0200

    Added avg price group, max avg price group

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,6 +3,7 @@ package app;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -18,6 +19,25 @@ public class Main {
         Map<String, List<Product>> groupedByCategory = products.stream()
                 .collect(Collectors.groupingBy(Product::getCategory));
 
+        System.out.println("\nПродукти мають такі категорії: ");
+        groupedByCategory.keySet().forEach(System.out::println);
 
+        // Знаходимо середнє арифметичне вартості в кожній категорії
+        Map<String, Double> averagePriceByCategory = products.stream()
+                .collect(Collectors.groupingBy(Product::getCategory, Collectors.averagingDouble(Product::getPrice)));
+
+        System.out.println("\nСередня ціна продуктів за категоріями: ");
+        averagePriceByCategory.forEach((category, averagePrice) -> {
+            System.out.printf("%s: %.2f%n", category, averagePrice);
+        });
+
+        // Знаходимо та виводимо максимальне середня значення вартості за категоріями
+        Optional<Map.Entry<String, Double>> maxAvgPriceCategory = averagePriceByCategory
+                .entrySet().stream().max(Map.Entry.comparingByValue());
+
+        System.out.println("\nКатегорія з найвищою середньою ціною: ");
+        maxAvgPriceCategory.ifPresent(entry -> {
+            System.out.printf("%s: %.2f", entry.getKey(), entry.getValue());
+        });
     }
 }
